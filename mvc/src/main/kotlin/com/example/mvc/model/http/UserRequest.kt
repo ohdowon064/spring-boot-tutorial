c71e8962157e514250c5c3e0625a706195e8b487
Dowon Oh <60056004+ohdowon064@users.noreply.github.com>
2021-05-21 01:58:21 +0900

    Costom Validation Annotation

--- mvc/src/main/kotlin/com/example/mvc/model/http/UserRequest.kt
+++ mvc/src/main/kotlin/com/example/mvc/model/http/UserRequest.kt
@@ -1,5 +1,6 @@
 package com.example.mvc.model.http
 
+import com.example.mvc.annotation.StringFormatDateTime
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.PropertyNamingStrategy
 import com.fasterxml.jackson.databind.annotation.JsonNaming
@@ -26,17 +27,19 @@ data class UserRequest(
 		@field:Pattern(regexp="^\\d{2,3}-\\d{3,4}-\\d{4}\$") // 정규식으로 전화번호 검증
 		var phoneNumber:String?=null,
 
+        @field:StringFormatDateTime(pattern="yyyy-MM-dd HH:mm:ss")
 		var createdAt:String?=null // yyyy-MM-dd HH:mm:ss ex) 2020-10-02 13:00:00
-	){
-
-		@AssertTrue(message="createdAt 패턴은 yyyy-MM-dd HH:mm:ss 입니다.") // 검증과정에서 실행되도록 하는 어노테이션
-		private fun isValidCreatedAt(): Boolean { // 메서드이므로 @field를 붙이지 않는다.
-                return try {
-                        LocalDateTime.parse(this.createdAt, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
-                        true
-                }catch (e:Exception) {
-                        false
-                }
-
-		}
-}
+)
+//	){
+//
+//		@AssertTrue(message="createdAt 패턴은 yyyy-MM-dd HH:mm:ss 입니다.") // 검증과정에서 실행되도록 하는 어노테이션
+//		private fun isValidCreatedAt(): Boolean { // 메서드이므로 @field를 붙이지 않는다.
+//                return try {
+//                        LocalDateTime.parse(this.createdAt, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+//                        true
+//                }catch (e:Exception) {
+//                        false
+//                }
+//
+//		}
+//}
